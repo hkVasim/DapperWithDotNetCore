8a160a5f2f1b845a90e90bbec8231eb1558d9dfe
hkVasim <ht.wasim@gmail.com>
2020-05-02 14:21:56 +0530

    Initital Commit

---
+++ DapperSolution/Controllers/EmlpoyeeController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DapperSolution.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperSolution.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        /// <summary>
+        /// Get All Employees.
+        /// </summary>
+        /// <returns>List of Employees.</returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<Employee>> GetEmployee()
+        {
+            var empList = EmployeeHelper.GetEmployeeList();
+            return this.Ok(empList);
+        }
+
+        /// <summary>
+        /// Get Employee by Id.
+        /// </summary>
+        /// <param name="id">EmployeeID.</param>
+        /// <returns>Employee Record.</returns>
+        [HttpGet("{id}")]
+        public ActionResult<Employee> GetEmployee(int id)
+        {
+            var employee = EmployeeHelper.GetEmployeeById(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
+
+        /// <summary>
+        /// Update Employee.
+        /// </summary>
+        /// <param name="id">Employee ID.</param>
+        /// <param name="employee">employee object.</param>
+        /// <returns>Update Employee Record.</returns>
+        [HttpPut("{id}")]
+        public IActionResult PutEmployee(int id, Employee employee)
+        {
+            if (id != employee.EmployeeId)
+            {
+                return BadRequest();
+            }
+            EmployeeHelper.UpdateEmployee(employee);
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Create New Employee.
+        /// </summary>
+        /// <param name="employee">employee.</param>
+        /// <returns>New Employee Record.</returns>
+        [HttpPost]
+        public ActionResult<Employee> PostEmployee(Employee employee)
+        {
+            EmployeeHelper.AddEmployee(employee);
+            return CreatedAtAction("GetEmployee", new { id = employee.EmployeeId }, employee);
+        }
+
+        /// <summary>
+        /// Delete employee.
+        /// </summary>
+        /// <param name="id">Employee ID.</param>
+        /// <returns>Delete Employee Record.</returns>
+        [HttpDelete("{id}")]
+        public ActionResult<Employee> DeleteEmployee(int id)
+        {
+            var employee = EmployeeHelper.GetEmployeeById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            EmployeeHelper.DeleteEmployee(id);
+            return employee;
+        }
+
+    }
+}
---
+++ DapperSolution/Models/EmployeeHelper.cs
@@ -0,0 +1,105 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperSolution.Models
+{
+    public class EmployeeHelper
+    {
+        static string strConnectionString { get; set; } = "Server=.;Database=EmployeeDB;Trusted_Connection=True;";
+        public static IEnumerable<Employee> GetEmployeeList()
+        {
+            List<Employee> employeeList = new List<Employee>();
+
+            using (IDbConnection con = new SqlConnection(strConnectionString))
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                employeeList = con.Query<Employee>("GetEmployeeList").ToList();
+            }
+
+            return employeeList;
+        }
+
+        public static Employee GetEmployeeById(int? id)
+        {
+            Employee employee = new Employee();
+            if (id == null)
+                return employee;
+
+            using (IDbConnection con = new SqlConnection(strConnectionString))
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                DynamicParameters parameter = new DynamicParameters();
+                parameter.Add("@Id", id);
+                employee = con.Query<Employee>("GetEmployeeByID", parameter, commandType: CommandType.StoredProcedure).FirstOrDefault();
+            }
+
+            return employee;
+        }
+
+        public static int AddEmployee(Employee employee)
+        {
+            int rowAffected = 0;
+            using (IDbConnection con = new SqlConnection(strConnectionString))
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@FirstName", employee.FirstName);
+                parameters.Add("@LastName", employee.LastName);
+                parameters.Add("@Email", employee.Email);
+                parameters.Add("@Age", employee.Age);
+
+                rowAffected = con.Execute("InsertEmployee", parameters, commandType: CommandType.StoredProcedure);
+            }
+
+            return rowAffected;
+        }
+
+        public static int UpdateEmployee(Employee employee)
+        {
+            int rowAffected = 0;
+
+            using (IDbConnection con = new SqlConnection(strConnectionString))
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@EmployeeId", employee.EmployeeId);
+                parameters.Add("@FirstName", employee.FirstName);
+                parameters.Add("@LastName", employee.LastName);
+                parameters.Add("@Email", employee.Email);
+                parameters.Add("@Age", employee.Age);
+                rowAffected = con.Execute("UpdateEmployee", parameters, commandType: CommandType.StoredProcedure);
+            }
+
+            return rowAffected;
+        }
+
+        public static int DeleteEmployee(int id)
+        {
+            int rowAffected = 0;
+            using (IDbConnection con = new SqlConnection(strConnectionString))
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@Id", id);
+                rowAffected = con.Execute("DeleteEmployee", parameters, commandType: CommandType.StoredProcedure);
+
+            }
+
+            return rowAffected;
+        }
+    }
+}
